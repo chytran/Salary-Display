61e114418b02aa8236291892446c2f6ecefafa37
Kevin Tran <kevintran1142@gmail.com>
2022-06-24 21:53:05 -0700

    Adding add salary page

---
+++ Salary-Display/AddingSalary.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddingSalary.aspx.cs" Inherits="Salary_Display._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <asp:Label ID="Label1" runat="server" Text="Overall Amount" onClick="loadData"></asp:Label>
+    <asp:GridView ID="SalaryGrid" runat="server"></asp:GridView>
+
+</asp:Content>
---
+++ Salary-Display/AddingSalary.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Salary_Display
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loadData(object sender, EventArgs e)
+        {
+            // Get connection String
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            // create sql connection to database
+            using(SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from salary", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    SalaryGrid.DataSource = reader;
+                    SalaryGrid.DataBind();
+                }
+            }
+        }
+    }
+}
