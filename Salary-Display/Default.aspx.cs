9d9b364c9002ee64b5d6904dde43339eaad10a9c
Kevin Tran <kevintran1142@gmail.com>
2022-07-01 17:29:10 -0700

    Automatically display salary

--- Salary-Display/Default.aspx.cs
+++ Salary-Display/Default.aspx.cs
@@ -13,7 +13,21 @@ namespace Salary_Display
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Get connection String
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
 
+            // create sql connection to database
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Name, Amount FROM salary", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    GridView1.DataSource = reader;
+                    GridView1.DataBind();
+                }
+            }
         }
 
         protected void onView(object sender, EventArgs e)
