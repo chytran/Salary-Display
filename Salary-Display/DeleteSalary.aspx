79085ccaf63161a1d9b1e20775d97d4f20d11dca
Kevin Tran <kevintran1142@gmail.com>
2022-07-11 21:33:03 -0700

    Delete salary completed

---
+++ Salary-Display/DeleteSalary.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DeleteSalary.aspx.cs" Inherits="Salary_Display.DeleteSalary" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <asp:Label ID="Label1" runat="server" Text="Overall Amount"></asp:Label>
+    <asp:TextBox ID="hours" runat="server"></asp:TextBox>
+    <asp:TextBox ID="salaryHourly" runat="server"></asp:TextBox>
+
+    <asp:Button ID="DeleteSalary" runat="server" Text="Button" onClick="loadData"/>
+
+    <asp:Label ID="display" runat="server" Text="$0.00"></asp:Label>
+</asp:Content>
---
+++ Salary-Display/DeleteSalary.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Salary_Display
+{
+    public partial class DeleteSalary : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loadData(object sender, EventArgs e)
+        {
+            // Get connection String
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            var totalAmount = int.Parse(hours.Text) * int.Parse(salaryHourly.Text);
+            // create sql connection to database
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE salary " +
+                    "SET Amount = Amount - " + totalAmount + " WHERE Name = 'Josh'", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                }
+            }
+        }
+
+        public void calculate(object sneder, EventArgs e)
+        {
+            var totalAmount = int.Parse(hours.Text) * int.Parse(salaryHourly.Text);
+            display.Text = "$" + totalAmount.ToString();
+        }
+    }
+}
